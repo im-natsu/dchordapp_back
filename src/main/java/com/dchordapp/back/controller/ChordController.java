5655f9f71b9945fa263175720072f9ffd9a4741b
im-natsu <n.haga@infomart.co.jp>
2025-02-27 16:19:16 +0900

    CrossOrigin設定

--- src/main/java/com/dchordapp/back/controller/ChordController.java
+++ src/main/java/com/dchordapp/back/controller/ChordController.java
@@ -3,6 +3,7 @@ package com.dchordapp.back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@CrossOrigin(origins = "https://dchordapp-front-1.onrender.com")
 public class ChordController{
  
   //private final DiatonicChordRepository diatonicChordRepository;
